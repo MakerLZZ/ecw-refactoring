1aec219318e8bdd706047dd6414661e5b1fd6da2
李真真 <731466760@qq.com>
2019-05-28 19:09:37 +0800

    增加路由配置 配置订单详情页路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import {Button} from 'antd';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import OrderDetail from './views/orderDetail'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
+                {/* <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
                         Edit
@@ -22,7 +25,11 @@ class App extends Component {
                         Learn React
                     </a>
                     <Button type="primary">Button</Button>
-                </header>
+                </header> */}
+                <Router>
+                    <div>顶部</div>
+                    <Route path="/orderDetail" render={() => <OrderDetail/>}/>
+                </Router>
             </div>
         )
     };
---
+++ src/views/orderDetail/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class OrderDetail extends Component {
+    render() {
+        return (
+            <div className=''>
+                订单详情
+            </div>
+        );
+    }
+};
+
+export default OrderDetail;
